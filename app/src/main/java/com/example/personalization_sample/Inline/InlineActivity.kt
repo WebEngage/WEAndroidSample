d3c6cef83de7b7e38d442eb651dff00e8b0dc40b
Akshay <akshay.chilad@webengage.com>
2023-05-26 15:04:33 +0530

    Created Main Activity with Buttons

---
+++ app/src/main/java/com/example/personalization_sample/Inline/InlineActivity.kt
@@ -0,0 +1,84 @@
+package com.example.personalization_sample.Inline
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RelativeLayout
+import com.example.personalization_sample.CustomScreen
+import com.example.personalization_sample.R
+import com.example.personalization_sample.SharedPrefsManager
+import com.example.personalization_sample.Utils
+import com.example.personalization_sample.model.DataModel
+import com.webengage.sdk.android.WebEngage
+
+class InlineActivity : AppCompatActivity() {
+    private lateinit var activityMain: RelativeLayout
+    private lateinit var editText: EditText
+    private lateinit var saveButton: Button
+    private lateinit var logoutButton: Button
+    private lateinit var customscreenButton: Button
+    val weUser = WebEngage.get().user()
+    var weAnalytics = WebEngage.get().analytics()
+
+    val prefs: SharedPrefsManager = SharedPrefsManager.get()
+
+
+    fun showLogin() {
+        editText.visibility = EditText.VISIBLE
+        saveButton.visibility = Button.VISIBLE
+        logoutButton.visibility = Button.GONE
+    }
+
+    fun showLogoutButton() {
+        editText.visibility = EditText.GONE
+        saveButton.visibility = Button.GONE
+        logoutButton.visibility = Button.VISIBLE
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inline)
+
+
+        val arr = Utils.getModelData()
+        DataModel.getInstance().updateData(arr)
+        activityMain = findViewById(R.id.activity_main)
+        editText = findViewById(R.id.editText)
+        saveButton = findViewById(R.id.button)
+        logoutButton = findViewById(R.id.logoutButton)
+        customscreenButton = findViewById(R.id.customScreenButton);
+        val strData = prefs.getString("registry", "")
+
+
+        val cuid = prefs.getString("cuid", "")
+        if (cuid.isNullOrEmpty()) {
+            showLogin();
+            saveButton.setOnClickListener {
+                val text = editText.text.toString()
+                prefs.put("cuid", text)
+                showLogoutButton()
+                weUser.login(text)
+            }
+        } else {
+            showLogoutButton()
+        }
+        customscreenButton.setOnClickListener {
+            val intent = Intent(this, CustomScreen::class.java)
+            startActivity(intent)
+        }
+
+        // Handle the Logout button click
+        logoutButton.setOnClickListener {
+            prefs.put("cuid", "")
+            weUser.logout()
+            showLogin()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        WebEngage.get().analytics().screenNavigated("main-screen")
+
+    }
+}
--- app/src/main/java/com/example/personalization_sample/MainActivity.kt
+++ app/src/main/java/com/example/personalization_sample/MainActivity.kt
@@ -11,71 +11,9 @@ import com.webengage.sdk.android.WebEngage
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var activityMain: RelativeLayout
-    private lateinit var editText: EditText
-    private lateinit var saveButton: Button
-    private lateinit var logoutButton: Button
-    private lateinit var customscreenButton: Button
-    val weUser = WebEngage.get().user()
-    var weAnalytics = WebEngage.get().analytics()
 
-    val prefs: SharedPrefsManager = SharedPrefsManager.get()
-
-
-    fun showLogin() {
-        editText.visibility = EditText.VISIBLE
-        saveButton.visibility = Button.VISIBLE
-        logoutButton.visibility = Button.GONE
-    }
-
-    fun showLogoutButton() {
-        editText.visibility = EditText.GONE
-        saveButton.visibility = Button.GONE
-        logoutButton.visibility = Button.VISIBLE
-    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-        val arr = Utils.getModelData()
-        DataModel.getInstance().updateData(arr)
-        activityMain = findViewById(R.id.activity_main)
-        editText = findViewById(R.id.editText)
-        saveButton = findViewById(R.id.button)
-        logoutButton = findViewById(R.id.logoutButton)
-        customscreenButton = findViewById(R.id.customScreenButton);
-        val strData = prefs.getString("registry", "")
-
-
-        val cuid = prefs.getString("cuid", "")
-        if (cuid.isNullOrEmpty()) {
-            showLogin();
-            saveButton.setOnClickListener {
-                val text = editText.text.toString()
-                prefs.put("cuid", text)
-                showLogoutButton()
-                weUser.login(text)
-            }
-        } else {
-            showLogoutButton()
-        }
-        customscreenButton.setOnClickListener {
-            val intent = Intent(this, CustomScreen::class.java)
-            startActivity(intent)
-        }
-
-        // Handle the Logout button click
-        logoutButton.setOnClickListener {
-            prefs.put("cuid", "")
-            weUser.logout()
-            showLogin()
-        }
-    }
-
-    override fun onStart() {
-        super.onStart()
-        WebEngage.get().analytics().screenNavigated("main-screen")
-
     }
 }
